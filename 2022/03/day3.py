fc4eef5e5ed0c1bc82a2262a47c134b3f3c17a43
Eric Roe <ericr@stcu.org>
2022-12-02 21:32:40 -0800

    Day 3, yay sets

---
+++ 2022/03/day3.py
@@ -0,0 +1,38 @@
+from collections import defaultdict, Counter, deque
+
+with open('./in.txt') as f:
+    d = [l.strip() for l in f.readlines()]
+
+
+def one_liner(): #I'm sorry lol
+    return sum(ord((set(r[:len(r)//2])&set(r[len(r)//2:])).pop()) - (96 if ord((set(r[:len(r)//2])&set(r[len(r)//2:])).pop()) > 96 else 64 - 26) for r in d)
+
+
+def p1():
+    common = []
+    total = 0
+    for r in d:
+        a = set(r[:len(r)//2])
+        b = set(r[len(r)//2:])
+        common.append((a&b).pop())
+    for l in common:
+        total += ord(l) - (96 if ord(l) > 96 else 64 - 26)
+    return total
+
+
+def p2():
+    common = []
+    total = 0
+    for A, B, C in zip(d[::3], d[1::3], d[2::3]):
+        a = set(A)
+        b = set(B)
+        c = set(C)
+        common.append((a&b&c).pop())
+    for l in common:
+        total += ord(l) - (96 if ord(l) > 96 else 64 - 26)
+    return total
+
+
+print(p1())
+print(one_liner())
+print(p2())
